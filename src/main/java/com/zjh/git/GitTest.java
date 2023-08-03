f92cac262b52039b90a7de68e5b6b327c086aa30
zjh <zjh@qq.com>
2023-08-03 09:41:40 +0800

    second2 commit

--- src/main/java/com/zjh/git/GitTest.java
+++ src/main/java/com/zjh/git/GitTest.java
@@ -2,6 +2,6 @@ package com.zjh.git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello world!");
+        System.out.println("hello world");
     }
 }
