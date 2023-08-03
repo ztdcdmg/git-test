5bb5ba0438f3833e99effda7218c395854c15b64
zjh <zjh@qq.com>
2023-08-03 09:52:28 +0800

    hot-fix commit1

--- src/main/java/com/zjh/git/GitTest.java
+++ src/main/java/com/zjh/git/GitTest.java
@@ -3,5 +3,6 @@ package com.zjh.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("commit3");
+        System.out.println("hello commit4");
     }
 }
