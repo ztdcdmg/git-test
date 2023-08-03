a06582bc17c5289111c707936492612a568afdb2
zjh <zjh@qq.com>
2023-08-03 09:57:03 +0800

    master commit2

--- src/main/java/com/zjh/git/GitTest.java
+++ src/main/java/com/zjh/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("commit3");
         System.out.println("hello commit4");
+        System.out.println("hello master!");
     }
 }
