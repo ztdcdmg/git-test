f11a005137be7b8307cb668d008e80e606a56274
zjh <zjh@qq.com>
2023-08-03 10:46:56 +0800

    push commit

--- src/main/java/com/zjh/git/GitTest.java
+++ src/main/java/com/zjh/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hello commit4");
         System.out.println("hello master!");
         System.out.println("hot-fix tset!");
+        System.out.println("push test!");
     }
 }
